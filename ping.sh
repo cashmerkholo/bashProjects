25a521e9b0306054c772fcf3019d0974c7a0bc5d
cashmerkholo <kilwazolduk505@gmail.com>
2024-06-04 12:31:41 +0100

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -9,7 +9,7 @@ echo "**************************************************************"
 echo "This script will get you all the hosts on your local network "
 echo "**************************************************************"
 for ip in $(seq 1 254); do
-ping -c 1 192.168.1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done 
 
 
